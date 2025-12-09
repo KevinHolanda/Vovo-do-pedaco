b5a1e9fe9fbb222cba1b2940b3e8d237dfea7982
KEVIN DE BRITO HOLANDA <45214308809@imaccommand026.local>
2025-12-09 14:20:42 -0300

    Quase feito

--- script.js
+++ script.js
@@ -73,3 +73,18 @@ onresize = () =>{
         iconeBarras.style.display = "inline"
     }
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
